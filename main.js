5b76cfbebe24b6f3948be61f19de42e81bfab0e7
Teri Castle <tea.@gmail.com>
2014-09-23 12:52:55 -0600

    array exercises

---
+++ main.js
@@ -0,0 +1,23 @@
+// Lecture 3
+// var myArray = ['Hello', 'World'] 
+
+// console.log( myArray[0] + myArray[1] );
+
+
+// Lecture 4
+// var myArray = ['Hello', 'World', '!', 'How are you?']
+// // for( initializer; condition; iterator ){}
+// for(var i = 0; i < myArray.length; i++){
+// 	console.log( myArray[i] );	
+// }
+
+// // Lecture 5
+// var myArray = [5, 10, 3, 10];
+
+// var result = 0
+
+// for(var i = 0; i < myArray.length; i++){
+// 	result += myArray[i];
+// }
+
+// console.log( result );
